febb47d6fe1c5932f27dbf5c1e8eb831004f65d1
Tarikkoc <ahmettarikkoc@gmail.com>
2023-06-14 11:56:34 +0300

    api commit

--- server.js
+++ server.js
@@ -1,23 +1,35 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-
+const fs = require("fs");
 const app = express();
 const cors = require("cors");
 const PORT = 5000;
 
-let data = [{ name: "A.Tarık", surname: "Koç", email: "atarikkoc@gmail.com" }];
 app.use(cors());
+app.use(bodyParser.json());
 // Temel bir GET endpoint'i
 app.get("/api", (req, res) => {
+  const jsonData = fs.readFileSync("./data/db.json", "utf-8");
+  const data = JSON.parse(jsonData);
   res.json(data);
 });
 
-app.use(bodyParser.json());
-
 app.post("/api", (req, res) => {
   const newData = req.body;
+  console.log("Gelen veriler", newData);
+
+  //db.jsonu oku
+  const jsonData = fs.readFileSync("./data/db.json", "utf8");
+  const data = JSON.parse(jsonData);
+
+  // Yeni veriyi ekle
   data.push(newData);
-  console.log("gelen veriler", newData);
+
+  // Verileri db.json dosyasına kaydet
+  const updatedJsonData = JSON.stringify(data, null, 2);
+  fs.writeFileSync("./data/db.json", updatedJsonData, "utf8");
+
+  console.log("veri kaydetme başarılı");
   res.status(200).json({ message: "veri başarıyla kaydedildi" });
 });
 
